6ba8dd3d688b199754163df0bc04d8769a6059a6
Wagner3UB <wagner.trezub@gmail.com>
2022-01-24 09:54:43 +0100

    script.js rinomiato a index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+/* 
+product display home
+<ul>
+  <li><img src="https://www.nonsprecare.it/wp-content/uploads/2018/12/benefici-delle-banane.jpg" alt="3 banane gialle"></li>
+  <li><h2>Nome della Frutta</h2><p>Descrizione, come collore, taglia, peso, etc.</p></li>
+  <li class="col-1"><h4>Prezzo:</h4><p>00,00</p><p>€</p></li>
+  <li class="col-1 add"><button>+</button><p>0</p><button>-</button></li>
+  <li class="col-1 color"><h4>Totale:</h4><p id="totalProduct">00,00</p><p>€</p></li>
+  <li class="col-1 add_to_cart"><button id="add_to_cart"><span class="material-icons-outlined">add_shopping_cart</span></button></li>
+</ul> */
+
+/* 
+product cart home
+<ul class="productCartItem">
+  <li>Nome del prodotto</li>
+  <li class="middleProductCart"><button>+</button><p>0</p><button>-</button></li>
+  <li class="lastProductCart"><p>00,00</p></li>
+</ul> */
+
+/*
+Product display ADM  
+<ul>
+  <li><img src="https://www.nonsprecare.it/wp-content/uploads/2018/12/benefici-delle-banane.jpg" alt="3 banane gialle"></li>
+  <li><h2>Nome della Frutta</h2><p>Descrizione, come collore, taglia, peso, etc.</p></li>
+  <li class="col-1"><h4>Prezzo:</h4><p>00,00</p><p>€</p></li>
+  <li class="col-1 add"><button>+</button><p>0</p><button>-</button></li>
+  <li class="col-1"><h4>Discount</h4><p id="discountProduct">20</p><p>%</p></li>
+  <li class="col-1"><button class="material-icons-outlined">remove_circle</button></li>
+</ul> */
